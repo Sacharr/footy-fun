b74ae44d2034c9f6affa6763847a9cdc0935cfd4
Nick <tatassupra98@hotmail.com>
2026-02-25 20:25:53 -0500

    created DbConext

---
+++ src/Footy-Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Footy_Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace Footy_Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Player> Players => Set<Player>();
+}

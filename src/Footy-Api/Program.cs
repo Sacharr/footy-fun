f1e291d49eaa2fb4f0f5c4f3988fd65b17672bd7
Nick <tatassupra98@hotmail.com>
2026-02-25 20:32:48 -0500

    registered the DbContext and applied migrations at startup

--- src/Footy-Api/Program.cs
+++ src/Footy-Api/Program.cs
@@ -1,13 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Footy_Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// apply migrations automatically on startup (optional for development)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

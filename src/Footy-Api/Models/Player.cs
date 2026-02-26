bb663635f755b8fc99166074517dde04768525d7
Nick <tatassupra98@hotmail.com>
2026-02-25 20:28:53 -0500

    created player model

---
+++ src/Footy-Api/Models/Player.cs
@@ -0,0 +1,9 @@
+﻿namespace Footy_Api.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Goals { get; set; }
+    }
+}
